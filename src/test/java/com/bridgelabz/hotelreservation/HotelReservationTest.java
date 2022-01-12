0a8fd82064f00bc82041e0b89762fdd5baef8ac6
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-12 21:17:16 +0530

    UPDATED: test case for best rated hotel

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -58,4 +58,15 @@ public class HotelReservationTest {
 		String hotelName = hotelReservation.getCheapestHotel(startDate, endDate);
 		Assert.assertEquals("Lakewood", hotelName);
 	}
+	@Test
+	public void givenHotelDetails_shouldReturnBestRatedHotel(){
+		
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("Lakewood", 3, 110, 90);
+		hotelReservation.addHotel("Bridgewood", 4, 160, 50);
+		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);    
+		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);    
+		String hotelName = hotelReservation.getBestRatedHotel(startDate, endDate);
+		Assert.assertEquals("Bridgewood", hotelName);
+	}
 }
