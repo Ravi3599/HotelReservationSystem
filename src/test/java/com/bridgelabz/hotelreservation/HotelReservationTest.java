e993042882508a37ea78d2de50a3159b1e0a4437
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-14 19:55:33 +0530

    UPDATED:test case cheapest best rated hotel for reward customer

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -14,7 +14,6 @@ public class HotelReservationTest {
 		String hotelName = hotelReservation.getHotelList().get(0).getHotelName();
 		Assert.assertEquals("Bridgewood", hotelName);
 	}
-	
 	@Test
 	public void givenHotelList_WhenAdded_shouldReturnProperHotelRating(){
 		HotelReservationIF hotelReservation = new HotelReservation();
@@ -22,7 +21,6 @@ public class HotelReservationTest {
 		int hotelRating = hotelReservation.getHotelList().get(0).getRating();
 		Assert.assertEquals(4, hotelRating);
 	}
-	
 	@Test
 	public void givenHotelList_WhenAdded_shouldReturnWeekDayRate(){
 		HotelReservationIF hotelReservation = new HotelReservation();
@@ -36,6 +34,20 @@ public class HotelReservationTest {
 		hotelReservation.addHotel("Bridgewood", 4, 220,150,110,50);
 		int weekendRate = (int) hotelReservation.getHotelList().get(0).getWeekendRate();
 		Assert.assertEquals(150, weekendRate);
+	}
+	@Test
+	public void givenHotelList_WhenAdded_shouldReturnWeekDayRewardRate(){
+		HotelReservationIF hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("Bridgewood", 4, 150,50,110,50);
+		int weekdayRewardRate = (int) hotelReservation.getHotelList().get(0).getWeekdayRewardCustomerRate();
+		Assert.assertEquals(110, weekdayRewardRate);
+	}
+	@Test
+	public void givenHotelList_WhenAdded_shouldReturnWeekendRewardRate(){
+		HotelReservationIF hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("Bridgewood", 4, 220,150,110,50);
+		int weekendRewardRate = (int) hotelReservation.getHotelList().get(0).getWeekendRewardCustomerRate();
+		Assert.assertEquals(50, weekendRewardRate);
 	}
 	@Test
 	public void givenHotelDetails_WhenSizeMatches_ShoulReturnTrue()
@@ -55,8 +67,8 @@ public class HotelReservationTest {
 		hotelReservation.addHotel("Bridgewood", 4, 160, 50,110,50);
 		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);    
 		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);    
-		String hotelName = hotelReservation.getCheapestHotel(startDate, endDate);
-		Assert.assertEquals("Lakewood", hotelName);
+		String hotelName = hotelReservation.getCheapestHotel(startDate, endDate,"reward");
+		Assert.assertEquals("Bridgewood", hotelName);
 	}
 	@Test
 	public void givenHotelDetails_shouldReturnBestRatedHotel(){
@@ -68,5 +80,42 @@ public class HotelReservationTest {
 		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);    
 		String hotelName = hotelReservation.getBestRatedHotel(startDate, endDate);
 		Assert.assertEquals("Bridgewood", hotelName);
+	}
+	@Test
+	public void givenHotelDetails_WhenNull_ShouldThrowHotelReservationException() {
+		
+		
+		try {
+			HotelReservation hotelReservation = new HotelReservation();
+			hotelReservation.addHotel("Lakewood", 3, 110, 90, 80, 80);
+			hotelReservation.addHotel("Bridgewood", 4, 150, 50, 110, 50);
+			hotelReservation.addHotel("Ridgewood", 5, 220, 150, 100, 40);
+			LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
+			LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);
+			hotelReservation.getCheapestHotel(startDate, endDate,null);
+		}
+		catch(HotelReservationException e){
+			Assert.assertEquals(HotelReservationException.ExceptionType.ENTERED_NULL, e.type);
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void givenHotelDetails_WhenEmpty_ShouldThrowHotelReservationException() {
+		
+		
+		try {
+			HotelReservation hotelReservation = new HotelReservation();
+			hotelReservation.addHotel("Lakewood", 3, 110, 90, 80, 80);
+			hotelReservation.addHotel("Bridgewood", 4, 150, 50, 110, 50);
+			hotelReservation.addHotel("Ridgewood", 5, 220, 150, 100, 40);
+			LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
+			LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);
+			hotelReservation.getCheapestHotel(startDate, endDate,"");
+		}
+		catch(HotelReservationException e){
+			Assert.assertEquals(HotelReservationException.ExceptionType.ENTERED_EMPTY, e.type);
+			e.printStackTrace();
+		}
+		
 	}
 }
